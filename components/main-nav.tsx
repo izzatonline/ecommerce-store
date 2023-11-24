8ba2a91c45a190c73b33da1f1489c0a96749a294
izzatonline <izzat.online@gmail.com>
2023-11-25 04:18:08 +0800

    final commit

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Category } from "@/types";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { Category } from "@/types";
 
 interface MainNavProps {
   data: Category[];
@@ -19,7 +21,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
 
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      {routes.map((route) => {
+      {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
@@ -29,8 +31,8 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
           )}
         >
           {route.label}
-        </Link>;
-      })}
+        </Link>
+      ))}
     </nav>
   );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
+import NavbarActions from "@/components/navbar-actions";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -14,6 +15,7 @@ const Navbar = async () => {
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
