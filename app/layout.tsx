99f7f169da58d28e08879e132e34e215efb213d4
izzatonline <izzat.online@gmail.com>
2023-11-27 14:01:52 +0800

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Thrieftied",
-  description: "Thrieftied - Your Haven for Budget Finds",
+  title: "Thriftied",
+  description: "Thriftied - Your Haven for Budget Finds",
 };
 
 export default function RootLayout({
